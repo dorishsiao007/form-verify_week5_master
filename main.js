3cce4abff3196d3d334b38ed5945ff0f267d0764
Doris Hsiao <dorishsiao007@gmail.com>
2020-07-24 14:21:43 +0800

    form format

---
+++ main.js
@@ -0,0 +1,33 @@
+// Import多國語系
+import zh_TW from './zh_TW.js';
+
+// 註冊驗證套件
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+
+// Class 設定檔案
+VeeValidate.configure({
+    classes: {
+      valid: 'is-valid',
+      invalid: 'is-invalid',
+    }
+});
+// 加入語系至 VeeValidate 的設定檔案
+VeeValidate.localize('tw', zh_TW);
+
+// 建立Vue實體
+new Vue({
+    el: '#app',
+    data: {
+        name: '',
+        mail: '',
+        phone: '',
+        addresse: '',
+        pay: '',
+        message: ''
+    },
+    methods: {
+        sendForm(){
+            alert('表單資料已送出');
+        }
+    }
+});
